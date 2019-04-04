83658770187a7cf192d01623ff5774f806e88ec7
SIRIJUS999 <48905473+SIRIJUS999@users.noreply.github.com>
2019-04-04 11:04:47 +0300

    apsirasiau suuma

---
+++ js/functions.js
@@ -0,0 +1,15 @@
+console.log('********************************************');
+console.log('********************************************');
+console.log('********************************************');
+console.log('*************FUNCTIONS**********************');
+console.log('********************************************');
+console.log('********************************************');
+console.log('********************************************');
+
+function tusciaFunkcija () {
+    return false;
+}
+console.log( tusciaFunkcija( ))                    //mes tokiu budu iskvieciame funkcija
+
+
+
--- js/test.js
+++ js/test.js
@@ -4,6 +4,16 @@ console.log('***************************************');
 console.log('***************************************');
 console.log('***************************************');
 
+// -0.03 = -3e-2
+// 1 = 1e0
+// 10 = 1e1
+// 100 = 1e2
+// 1000 = 1e3
+// 2019 = 2.019e3
+// 1000000 = 1e6
+
+
+
 // 1----------> Kintamųjų inicijavimas
 
 //  1.1--->    Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmi
@@ -236,7 +246,7 @@ var skaiciai = [0,1,2,3,4];
 
 
 for ( var i=0; i<skaiciai.length ; i++ ) {
-                                                        // suma = suma + skaiciai[i];
+                               // suma = suma + skaiciai[i];                         
     suma += skaiciai[i];
 
     
@@ -283,3 +293,145 @@ console.log( 'suma: ' + suma );
 
 
 //                                                 console.log( 'suma: ' + suma );
+
+
+var suma_2 = 0;
+var skaiciai_2 = [0,1,2,3,4];
+
+for ( var i=25; i<30; i++ ) {
+    suma_2 += skaiciai_2[i-25];
+    console.log( i + ' - ' + suma_2 );
+}
+console.log( 'suma_2: ' + suma_2 );
+
+console.log( '**************************************************************' );
+
+var suma = 0;                                                                //<---------- LABAI GERAI  
+var nuo = 0;                                                                 //     SURASYTAS PAVIZDYS                            
+var iki = 10;
+for ( var i=nuo; i<=iki; i++ ) {
+    suma += i;
+}
+console.log( 'Suma intervale [' + nuo + ' - ' + iki + '] yra ' + suma );
+
+console.log( '**************************************************************' );
+console.log( '**************************************************************' );
+console.log( '**************************************************************' );
+
+
+var pradinis_tekstas = 'Labas rytas :)';
+var galutinis_tekstas = '';
+var ilgis = pradinis_tekstas.length;
+
+for ( var i=0; i<ilgis; i++ )  {
+    galutinis_tekstas += pradinis_tekstas[ilgis-i-1];
+}
+// pradinis_tekstas = abcd
+// ilgis = 4
+// 0 - 'd'
+// 1 - 'dc'
+// 2 - 'dcb'
+// 3 - 'dcba'
+console.log( galutinis_tekstas );
+
+
+console.log( '**************************************************************' );
+console.log( '**************************************************************' );
+console.log( '**************************************************************' );
+console.log( '**************************************************************' );
+console.log( '****************pirmas budas kuris man nelb patinka***********' );
+console.log( '******ir kai eina kalba apie milziniskus skaicius neisnasa****' );
+
+
+var   t = 1;
+var nuo = 0;
+var iki = 15;
+var be_liekanos_3 = 0;
+var be_liekanos_5 = 0;
+var be_liekanos_7 = 0;
+
+// ciklas FOR
+for ( var i=nuo; i<=iki; i++ ) {
+    // tikriname ar einamasis skaicius dalinasi is 3 be liekanos
+    if ( i%3 === 0 ) { 
+        be_liekanos_3++; 
+    } 
+}    
+
+var be_liekanos_3 = be_liekanos_3 - 1; 
+
+console.log( 'Skaičių intervale tarp '+nuo+' ir '+iki+', besidalinančių be liekanos iš 3 yra '+be_liekanos_3+' vienetai.' );
+
+for ( var i=nuo; i<=iki; i++ ) {
+    // tikriname ar einamasis skaicius dalinasi is 5 be liekanos
+    if ( i%5 === 0 ) {
+        be_liekanos_5++;
+    }
+}
+console.log( 'Skaičių intervale tarp '+nuo+' ir '+iki+', besidalinančių be liekanos iš 5 yra '+be_liekanos_5+' vienetai.' );
+
+for ( var i=nuo; i<=iki; i++ ) {
+    // tikriname ar einamasis skaicius dalinasi is 7 be liekanos
+    if ( i%7 === 0 ) {
+        be_liekanos_7++;
+    }
+}
+console.log( 'Skaičių intervale tarp '+nuo+' ir '+iki+', besidalinančių be liekanos iš 7 yra '+be_liekanos_7+' vienetai.' );
+
+console.log( '*******************KITAS BUDAS ATLIKTI TAPATI******************' );
+
+
+var nuo = 0;
+// var iki = 150000000;
+var iki = 150;
+var dalikliai = [2,3,5,7,11];
+var be_liekanos = 0;
+
+for ( var d=0; d<dalikliai.length; d++ ) {
+    be_liekanos = 0;
+    for ( var i=nuo; i<=iki; i++ ) {
+        if ( i % dalikliai[d] === 0 ) {
+            be_liekanos++;
+        }
+    }
+    console.log( 'Skaičių intervale tarp '+nuo+' ir '+iki+', besidalinančių be liekanos iš '+dalikliai[d]+' yra '+be_liekanos+' vienetai.' );
+}
+
+console.log( '*******************DAR KITAS BUDAS ATLIKTI TAPATI******************' );
+console.log( '*******************DAR KITAS BUDAS ATLIKTI TAPATI******************' );
+console.log( '*******************DAR KITAS BUDAS ATLIKTI TAPATI******************' );
+console.log( '*******************DAR KITAS BUDAS ATLIKTI TAPATI******************' );
+
+
+var nuo = 0;
+var iki = 15;
+// var dalikliai = [2,3,5,7,11];
+var dalikliai = [3];
+var be_liekanos = 0;
+var intervalo_dydis = iki - nuo;
+var ats_pilnas = 0;
+var ats_pradzia = 0;
+var ar_dubliuojasi = 0;
+
+for ( var d=0; d<dalikliai.length; d++ ) {
+    if ( dalikliai[d] !== 0 ) {
+        ats_pilnas = (iki - iki % dalikliai[d]) / dalikliai[d] + 1;
+        ats_pradzia = (nuo - nuo % dalikliai[d]) / dalikliai[d] + 1;
+        ar_dubliuojasi = 0;
+        if ( nuo % dalikliai[d] === 0 ) {
+            ar_dubliuojasi++;
+        }
+        if ( nuo < 0 ) {
+            if ( nuo % dalikliai[d] !== 0 ) {
+                ar_dubliuojasi++;
+            }
+        }
+        if ( iki < 0 ) {
+            if ( iki % dalikliai[d] !== 0 ) {
+                ar_dubliuojasi--;
+            }
+        }
+        var ats = ats_pilnas - ats_pradzia + ar_dubliuojasi;
+        console.log( 'Skaičių intervale tarp '+nuo+' ir '+iki+', besidalinančių be liekanos iš '+dalikliai[d]+' yra '+ats+' vienetai.' );
+    }
+}
